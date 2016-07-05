6d6114004aa46c9f320e8cf0455cc68c0d6278ec
MainShayne233 <shaynetremblay@hotmail.com>
2016-07-04 22:51:33 -0400

    added heorku action cable necessaties

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://letspairtogether.herokuapp.com/cable"
+
+  config.action_cable.allowed_request_origins = ['https://letspairtogether.herokuapp.com', 'http://letspairtogether.herokuapp.com']
+
 end
