8409fe7920c3846893993ddbce1ec34cbb171cf9
MainShayne233 <shaynetremblay@hotmail.com>
2016-07-04 23:01:58 -0400

    now uses eval to run ruby code, returns return value

--- app/models/whiteboard.rb
+++ app/models/whiteboard.rb
@@ -2,13 +2,18 @@ class Whiteboard < ApplicationRecord
   belongs_to :coderoom
 
   def run
-    base_path = "tmp/script_at_#{Time.now.to_i}"
-    file_path = "#{base_path}.rb"
-    output_path = "#{base_path}_output.txt"
-    File.open(file_path, 'w') {|file| file.write(content)}
-    %x(ruby #{Rails.root}/#{file_path} > #{output_path} 2>&1)
-    result = File.read(output_path).gsub("#{Rails.root}/#{file_path}", 'Line ')
-    File.delete file_path, output_path
-    result
+    # base_path = "tmp/script_at_#{Time.now.to_i}"
+    # file_path = "#{base_path}.rb"
+    # output_path = "#{base_path}_output.txt"
+    # File.open(file_path, 'w') {|file| file.write(content)}
+    # %x(ruby #{Rails.root}/#{file_path} > #{output_path} 2>&1)
+    # result = File.read(output_path).gsub("#{Rails.root}/#{file_path}", 'Line ')
+    # File.delete file_path, output_path
+    # result
+    begin
+      return eval(content)
+    rescue Exception => e
+      return e.to_s
+    end
   end
 end
