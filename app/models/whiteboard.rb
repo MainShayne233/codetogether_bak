9fc43b6261abb5ea1d58e57f71427b9a8fccdc77
MainShayne233 <shaynetremblay@hotmail.com>
2016-07-09 23:09:13 -0400

    simplified code execution

--- app/models/whiteboard.rb
+++ app/models/whiteboard.rb
@@ -3,27 +3,25 @@ class Whiteboard < ApplicationRecord
 
   def run!
     script_path = "tmp/script_from_#{user_id}_at_#{Time.now.to_i}.rb"
-    embedded_script_path = "tmp/embedded_script_from_#{user_id}_at_#{Time.now.to_i}.rb"
     output_path = "tmp/output_from_#{user_id}_at_#{Time.now.to_i}.txt"
-    embedded_output_path = "tmp/embedded_output_from_#{user_id}_at_#{Time.now.to_i}.txt"
-    File.write script_path, content
-    begin
-      `ruby #{script_path} > #{output_path}`
-      std_out = File.read(output_path)
-      File.write embedded_script_path, "print eval(%q(#{one_line_content}))"
-      `ruby #{embedded_script_path} > #{embedded_output_path}`
-      std_out << "> #{File.read(embedded_output_path)[std_out.size..-1]}"
-      puts File.read(embedded_output_path)
-    rescue Exception => e
-      byebug
-      std_out = e.to_s
-    ensure
-      # FileUtils.rm script_path
-      # FileUtils.rm output_path
-    end
+
+    script = "begin; "
+    script << "result = eval(%q(#{one_line_content})); "
+    script << "print '> ' + result.to_s unless result.nil?; "
+    script << "rescue Exception => e; puts e.to_s; "
+    script << "end"
+
+    File.write script_path, script
+    `ruby #{script_path} > #{output_path}`
+    script_output = File.read output_path
+    script_output = script_output[0..-2] if script_output[-1] == "\n"
+    std_out = script_output
+
     self.output << "\n" unless self.output.blank?
     self.output << Whiteboard.formatted_output(std_out)
     self.save
+
+    File.delete script_path, output_path
   end
 
   private
@@ -33,10 +31,17 @@ class Whiteboard < ApplicationRecord
   end
 
   def self.formatted_output output
-    output = output.to_s
-    lines = output.size / 80
-    (0..lines).each {|i| output.insert( ((i+1)*80), "\n") unless output.size < ((i+1)*80)}
-    output
+    lines = output.split("\n")
+    lines.each do |line|
+      breaks = line.size / 80
+      (0..breaks).each {|i| line.insert( ((i+1)*80), "\n") unless line.size < ((i+1)*80)}
+    end
+    lines.join "\n"
+  end
+
+  def safe?
+    return false if content.include? %w(`)
+    return false if content.include? %w()
   end
 
 
